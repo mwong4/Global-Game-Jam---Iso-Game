919465d00a5b592b1e258e7781fb1f990a5c5c60
Max <wong.max.c@gmail.com>
2019-07-13 15:31:21 -0400

    Added more to move

--- Cube&Paint/Assets/MasterMovement.cs
+++ Cube&Paint/Assets/MasterMovement.cs
@@ -14,9 +14,9 @@ public class MasterMovement : MonoBehaviour
     {
         board = new int[15, 15];
 
-        for (int i = 0; i > 15; i++)
+        for (int i = 0; i < 15; i++)
         {
-            for (int j = 0; j > 15; j++)
+            for (int j = 0; j < 15; j++)
             {
                 board[i, j] = 0;
             }
@@ -31,12 +31,16 @@ public class MasterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("w"))
+        if (Input.GetKeyDown("s"))
         {
             board[(int)playerPosition.x, (int)playerPosition.y] = 0;
             destinationPosition = new Vector2(playerPosition.x, playerPosition.y + 1);
 
-            checkBall(1);
+            if (checkSide(playerPosition, 1) == true)
+            {
+                checkBall(1);
+            }
+
 
         }
         if (Input.GetKeyDown("a"))
@@ -44,44 +48,55 @@ public class MasterMovement : MonoBehaviour
             board[(int)playerPosition.x, (int)playerPosition.y] = 0;
             destinationPosition = new Vector2(playerPosition.x - 1, playerPosition.y);
 
-            checkBall(2);
+            if(checkSide( playerPosition, 2 ) == true)
+            {
+                checkBall(2);
+            }
+
+
         }
-        if (Input.GetKeyDown("s"))
+        if (Input.GetKeyDown("w"))
         {
             board[(int)playerPosition.x, (int)playerPosition.y] = 0;
             destinationPosition = new Vector2(playerPosition.x, playerPosition.y - 1);
 
-            checkBall(3);
+            if (checkSide(playerPosition, 3) == true)
+            {
+                checkBall(3);
+            }
+
+
         }
         if (Input.GetKeyDown("d"))
         {
             board[(int)playerPosition.x, (int)playerPosition.y] = 0;
             destinationPosition = new Vector2(playerPosition.x + 1, playerPosition.y);
 
-            checkBall(4);
-        }
-
-        for (int i = 0; i > 15; i++)
-        {
-            for (int j = 0; j > 15; j++)
+            if (checkSide(playerPosition, 4) == true)
             {
-                Debug.Log(board[i, j]);
-
+                checkBall(4);
             }
+
+
         }
 
-        Debug.Log("Hello World!");
-
+        if(checkWin())
+        {
+            Debug.Log("Someone Wins");
+        }
 
     }
 
     void checkBall(int dir)
     {
+        Debug.Log("Direction: " + dir);
         if (board[(int)destinationPosition.x, (int)destinationPosition.y] == 2)
         {
+            
             if (dir == 1)
             {
                 board[(int)destinationPosition.x, (int)destinationPosition.y + 1] = 2;
+                
 
                 board[(int)destinationPosition.x, (int)destinationPosition.y] = 1;
             }
@@ -104,6 +119,95 @@ public class MasterMovement : MonoBehaviour
                 board[(int)destinationPosition.x, (int)destinationPosition.y] = 1;
             }
 
+        }else
+        {
+            if (dir == 1)
+            {
+                board[(int)destinationPosition.x, (int)destinationPosition.y] = 1;
+            }
+            if (dir == 2)
+            {
+                board[(int)destinationPosition.x, (int)destinationPosition.y] = 1;
+            }
+            if (dir == 3)
+            {
+                board[(int)destinationPosition.x, (int)destinationPosition.y] = 1;
+            }
+            if (dir == 4)
+            {
+                board[(int)destinationPosition.x, (int)destinationPosition.y] = 1;
+            }
+        }
+
+        playerPosition = new Vector2(destinationPosition.x, destinationPosition.y);
+
+        for (int i = 0; i < 15; i++)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                if(board[i, j] == 1)
+                {
+                    Debug.Log("person [" + i + ", " + j + "]");
+                }
+                if (board[i, j] == 2)
+                {
+                    Debug.Log("ball [" + i + ", " + j + "]");
+                }
+            }
         }
     }
+
+    bool checkSide( Vector2 playerPosition, int direction)
+    {
+        if(direction == 1 && playerPosition.y == 13)
+        {
+            return false;
+        }
+        if (direction == 2 && playerPosition.x == 1)
+        {
+            return false;
+        }
+        if (direction == 3 && playerPosition.y == 1)
+        {
+            return false;
+        }
+        if (direction == 4 && playerPosition.x == 13)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    bool checkWin()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            if (board[0, i] == 2)
+            {
+                Debug.Log("Left wins");
+                return true;
+            }
+            if (board[14, i] == 2)
+            {
+                Debug.Log("Right wins");
+                return true;
+            }
+            if (board[i, 0] == 2)
+            {
+                Debug.Log("Top wins");
+                return true;
+            }
+            if (board[i, 14] == 2)
+            {
+                Debug.Log("Bottom wins");
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
 }
